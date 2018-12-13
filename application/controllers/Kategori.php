7aee255d9e93839806b60ca88e895566369df8f5
Aldhan Biuzar Yahya <aldansorry@gmail.com>
2018-12-13 11:38:07 +0700

    get kategori

---
+++ application/controllers/Kategori.php
@@ -0,0 +1,14 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . '/libraries/REST_Controller.php';
+use Restserver\Libraries\REST_Controller;
+
+class Kategori extends REST_Controller {
+
+	function kategori_get(){
+		$get_kategori = $this->db->get("kategori")->result();
+		$this->response(array("status"=>"success","result" => $get_kategori));
+	}
+	
+}
